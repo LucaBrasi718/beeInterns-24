1be4aaa659ee073f3621fa3a4134a319b211f6cf
Rashid <mr.alhoev@mail.ru>
2020-06-02 21:49:20 +0300

    optimized selector

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const triangle = document.querySelector(".triangle");
+const input = speedInput;
 
-speedInput.addEventListener("change", changeSpeed);
-triangle.style.animationDuration = `${20 / speedInput.value}s`;
+input.addEventListener("change", changeSpeed);
+triangle.style.animationDuration = `${20 / input.value}s`;
 
 function changeSpeed() {
-    speed = 20 / speedInput.value;
+    speed = 20 / input.value;
     triangle.style.animationDuration = `${speed}s`;
 }
